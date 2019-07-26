0add4c2fd213c3561a414ebd2ef46c53316e0867
Bartlomiej Pabisiak <bartlomiej.pabisiak@gmail.com>
2019-07-26 10:52:30 +0200

    added basic model to the timetrack

--- EmployeeActivityManager/timetracker/models.py
+++ EmployeeActivityManager/timetracker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+
+class WorkingHours(models.Model):
+    work_begins = models.BooleanField()
+    work_ends = models.BooleanField()
+    time = models.DateTimeField()
+    employee_id = models.IntegerField(max_length=32)
+
+
